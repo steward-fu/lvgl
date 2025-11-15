f6589f4bfcd6577f3e0ee894b3c26a55a756aaf0
steward-fu <steward.fu@gmail.com>
2025-11-15 18:42:22 +0800

    Added freetype source.

---
+++ lv_conf.h
@@ -0,0 +1,6 @@
+#define LV_USE_SDL              1
+#define LV_USE_FREETYPE         1
+#define LV_USE_FONT_UTF8        1
+#define SDL_HOR_RES             320
+#define SDL_VER_RES             240
+#define LV_SDL_INCLUDE_PATH     <SDL2/SDL.h>
